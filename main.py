1433cb0df32785af32c3bd9b5c16dd29ed7aef53
Thang Nguyen <109894560+thanguyen165@users.noreply.github.com>
2022-12-28 20:20:28 +0700

    update week 2 (add functions)

--- main.py
+++ main.py
@@ -20,6 +20,41 @@ def load_mnist(path, kind='train'):
 
     return images, labels
 
+def Flatten(matrix):
+    arr = np.array(matrix)
+    return arr.flatten()
+
+def Average(matrix, r, c): # matrix - rows - columns
+    arr = np.array(matrix)
+    n = arr.shape[0]
+    m = arr.shape[1]
+    
+    a = (n + r - 1) // r # caculate new number of rows
+    b = (m + c - 1) // c # caculate new number of columns
+    res = np.zeros((a, b))
+    
+    for i in range(n):
+        for j in range(m):
+            res[i // r][j // c] += arr[i][j]
+            
+    for i in range(a):
+        for j in range(b):
+            res[i][j] //= (r * c)
+    
+    return res
+
+def Histogram(matrix):
+    arr = np.array(matrix)
+    res = np.zeros((256))
+    
+    for i in arr:
+        for j in i:
+            res[int(j)] += 1
+            
+    print(res)
+
+#____________________________________________________________________#
+
 X_train, y_train = load_mnist('data/', kind='train')
 print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
 
@@ -27,6 +62,7 @@ fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,)
 ax = ax.flatten()
 for i in range(10):
     img = X_train[y_train == i][0]
+    #img = Average(img, 2, 2)
     ax[i].imshow(img, cmap='Greys', interpolation='nearest')
 
 ax[0].set_xticks([])
