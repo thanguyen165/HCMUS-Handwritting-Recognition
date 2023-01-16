c8b92bac629c69c2dd8072c9ee7b8838985880c2
Thang Nguyen <109894560+thanguyen165@users.noreply.github.com>
2023-01-16 23:43:09 +0700

    clean the code

--- lib.cpp
+++ lib.cpp
@@ -1,36 +1,42 @@
 #include "lib.hpp"
-#include <stdio.h>
 #include <algorithm>
 
-const int N = (int) 6e4, sz = 28 * 28;
-node dis[N];
+const int X_TRAIN_SIZE = (int) 6e4;
+const int X_TEST_SIZE = (int) 1e4;
+node dis[X_TRAIN_SIZE];
+int cnt[10];
 
-int guess(void * xav, void * yav, void * testav) {
-    const int K = 1000;  // KNN
-    // must be unsigned long long type
-    unsigned long long * x = (unsigned long long *) xav;
-    unsigned long long * y = (unsigned long long *) yav;
-    unsigned long long * test = (unsigned long long *) testav;
+unsigned long long distance(void *a, void *b, int size) {
+    unsigned long long *a_ = (unsigned long long *) a;
+    unsigned long long *b_ = (unsigned long long *) b;
 
-    for (int i = 0; i < N; i++) {
-        unsigned long long sum = 0;
-        for (int j = 0; j < sz; j++) {
-            unsigned long long a = x[i * sz + j], b = test[j];
-            sum += (a - b)*(a - b);
-        }
-        dis[i].distance = sum;
-        dis[i].label = y[i];
+    unsigned long long ans = 0;
+    for (int i = 0; i < size; i++) {
+        ans += (a_[i] - b_[i])*(a_[i] - b_[i]);
+    }
+    return ans;
+}
+
+int guess_optimize(void *x_train_p, void *y_train_p, void *test_matrix_p, int img_size, int KNN) {
+    unsigned long long * x_train = (unsigned long long *) x_train_p;
+    unsigned long long * y_train = (unsigned long long *) y_train_p;
+    unsigned long long * test = (unsigned long long *) test_matrix_p;
+
+    for (int i = 0; i < X_TRAIN_SIZE; i++) {
+        dis[i].distance = distance((x_train + i*img_size), test, img_size);
+        dis[i].label = y_train[i];
     }
 
-    std::sort(dis, dis + N);
+    std::sort(dis, dis + X_TRAIN_SIZE);
 
-    int cnt[10];  // counting array
     for (int i = 0; i < 10; i++) cnt[i] = 0;
-    for (int i = 0; i < K; i++) cnt[dis[i].label]++;
+    for (int i = 0; i < KNN; i++) cnt[ dis[i].label ]++;
     
     int max_index = 0;
     for (int i = 0; i < 10; i++) {
-        if (cnt[i] > cnt[max_index]) max_index = i;
+        if (cnt[i] > cnt[max_index]) {
+            max_index = i;
+        }
     }
     return max_index;
 }
